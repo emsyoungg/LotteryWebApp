46c4362639c4e5ce4dc63ac18ff6c6f1fad3c57d
emsyoungg <emily03young@gmail.com>
2023-11-03 20:04:33 +0000

    1.2 Firstname and Lastname must not contain the characters:  * ? ! ' ^ + % & / ( ) = } ] [ { $ # @ < >

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, Email;
+from wtforms.validators import DataRequired, Email, ValidationError;
+
+
+def character_check(form, field):
+    exclude_chars = "*?!'^+%&/()=}][{$#@<>"
+
+    for char in field.data:
+        if char in exclude_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
     email = EmailField(validators=[Email()])
-    firstname = StringField()
-    lastname = StringField()
+    firstname = StringField(validators=[character_check])
+    lastname = StringField(validators=[character_check])
     phone = StringField()
     password = PasswordField()
     confirm_password = PasswordField()
