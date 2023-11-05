cfc9ee4b64d98effb18500987c7c59054256fe8c
emsyoungg <emily03young@gmail.com>
2023-11-05 15:39:28 +0000

    1.3 Phone number must be a String containing only digits (X) and dashes (-) of the form: XXXX-XXX-XXXX

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, Email, ValidationError;
+import re;
 
 
-def character_check(form, field):
+def character_check(self, field):
     exclude_chars = "*?!'^+%&/()=}][{$#@<>"
 
     for char in field.data:
         if char in exclude_chars:
             raise ValidationError(f"Character {char} is not allowed.")
 
+def is_valid_phone_number(self, phone):
+    pattern = re.compile(r'^\d{4}-\d{3}-\d{4}$')
+    if not pattern.match(phone.data):
+        raise ValidationError(f"Phone number must be in form XXXX-XXX-XXXX")
+
+
 
 class RegisterForm(FlaskForm):
     email = EmailField(validators=[Email()])
     firstname = StringField(validators=[character_check])
     lastname = StringField(validators=[character_check])
-    phone = StringField()
+    phone = StringField(validators=[is_valid_phone_number])
     password = PasswordField()
     confirm_password = PasswordField()
     submit = SubmitField()
