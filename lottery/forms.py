fbaa223d873cbc1b8156bdd725bc14f965b0ff66
emsyoungg <emily03young@gmail.com>
2023-11-22 14:57:05 +0000

    comments

--- lottery/forms.py
+++ lottery/forms.py
@@ -15,10 +15,6 @@ class DrawForm(FlaskForm):
                 self.number1.errors.append('Numbers must be unique.')
         return False
 
-    """def is_valid_number(self, number):
-        if not (0 < number.data < 61):
-            raise ValidationError(f"Number must be between 1 and 60.")"""
-
     number1 = IntegerField(id='no1', validators=[DataRequired("There must be 6 entries"), NumberRange(min=1, max=60, message="Number must be between 1 and 60.")])
     number2 = IntegerField(id='no2', validators=[DataRequired("There must be 6 entries"), NumberRange(min=1, max=60, message="Number must be between 1 and 60.")])
     number3 = IntegerField(id='no3', validators=[DataRequired("There must be 6 entries"), NumberRange(min=1, max=60, message="Number must be between 1 and 60.")])
